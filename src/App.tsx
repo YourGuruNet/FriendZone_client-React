3399c31767acf3d7d4e84aee70c8c28163811d2f
yourgurunet <info@yourgurunet.com>
2021-01-20 12:46:58 +0200

    Getting a response from API, and showed data on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-const App: React.FC = () => {
-  return (
-    <div >
-      Hello world from Typescript
-    </div>
-  );
+class App extends Component  {
+  state = {
+    values: []
+  }
+  componentDidMount() {
+axios.get('http://localhost:5000/api/values').then((response) =>{
+  console.log(response);
+  this.setState({
+    values: response.data
+  })
+})
+   
+  }
+  render() {
+    return (
+      <div >
+        <ul>
+          {this.state.values.map((value: any) => {
+            return <li key={value.id}>{value.name}</li>
+          })}
+        </ul>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
