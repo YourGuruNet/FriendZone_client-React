6b23159d8ab8b88272239e89b36e668dafa88bea
yourgurunet <info@yourgurunet.com>
2021-02-04 15:27:13 +0200

    Created a separate Home section page

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import FullViewActivity from '../../components/activities/FullViewActivity';
 import NavigationBar from '../../components/NavigationBar';
 import Activities from '../../pages/Activities';
@@ -8,18 +8,24 @@ import Home from '../../pages/Home';
 const App = () => {
   return (
     <Fragment>
-      <NavigationBar />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/activities'>
-          <Activities />
-        </Route>
-        <Route path='/activity/:id'>
-          <FullViewActivity />
-        </Route>
-      </Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <Fragment>
+            <NavigationBar />
+
+            <Route path='/activities'>
+              <Activities />
+            </Route>
+            <Route path='/activity/:id'>
+              <FullViewActivity />
+            </Route>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 const Home = () => {
   return (
-    <div>
-      <h1>Hello from home page</h1>
-      <h1>Hello from home page</h1>
-      <h1>Hello from home page</h1>
-      <h1>Hello from home page</h1>
-      <h1>Hello from home page</h1>
-      <h1>Hello from home page</h1>
-    </div>
+    <Container>
+      <h1>
+        Go to Activities <Link to='/activities'>Login</Link>
+      </h1>
+    </Container>
   );
 };
 
 export default Home;
+
+const Container = styled.div`
+  text-align: center;
+  font-size: 2rem;
+`;
