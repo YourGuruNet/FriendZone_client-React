d3854e8220f06d873ed83d7921e4865ecfbcd364
arvis <info@yourgurunet.com>
2021-03-05 12:46:43 +0200

    Home screen styles created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Logo1 from '../assets/logo1.png';
 const Home = () => {
   return (
     <Container>
-      <h1>
-        Go to Activities <Link to='/activities'>Login</Link>
-      </h1>
+      <img src={Logo1} alt='friend zone' />
+      <h1 className='home_title'>Welcome at FriendZone!</h1>
+      <Link className='home_button' to='/activities'>
+        Go to Activities list
+      </Link>
     </Container>
   );
 };
@@ -14,6 +17,37 @@ const Home = () => {
 export default Home;
 
 const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: var(--lightGradient);
   text-align: center;
   font-size: 2rem;
+
+  .home_title {
+    color: var(--baseColor-Light);
+  }
+  .home_button {
+    padding: 0.5rem 1rem;
+    margin-left: 1rem;
+    background-color: var(--baseColor-Dark-2);
+    border: solid 0.1rem var(--baseColor-Dark-2);
+    border-radius: 0.5rem 0.5rem 0.5rem 0;
+    text-transform: uppercase;
+    color: var(--baseColor);
+    font-size: 2.5rem;
+    transition: all 0.3s;
+    text-decoration: none;
+    cursor: pointer;
+    outline: none;
+    :hover {
+      background-color: var(--baseColor);
+      color: var(--baseColor-Dark-2);
+    }
+    :active {
+      transform: scale(0.9);
+    }
+  }
 `;
