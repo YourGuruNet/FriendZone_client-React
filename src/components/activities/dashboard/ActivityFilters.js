f9f47ebdc3c59503a17ecd97dca949c3a38a3aec
arvis <info@yourgurunet.com>
2021-03-05 11:10:06 +0200

    React calendar styls

--- src/components/activities/dashboard/ActivityFilters.js
+++ src/components/activities/dashboard/ActivityFilters.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import FilterCalendar from './FilterCalendar';
-
+import styled from 'styled-components';
 const ActivityFilters = () => {
   return (
-    <div>
+    <Section>
       <FilterCalendar />
-    </div>
+    </Section>
   );
 };
 
 export default ActivityFilters;
+
+const Section = styled.section`
+  background-color: var(--baseColor-dark);
+  width: 100%;
+  margin-top: 3rem;
+  overflow: hidden;
+  position: relative;
+`;
--- src/components/activities/dashboard/FilterCalendar.js
+++ src/components/activities/dashboard/FilterCalendar.js
@@ -1,14 +1,45 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { Popup } from '../../../app/layout/styles';
+import styled from 'styled-components';
 
 const FilterCalendar = () => {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div>
-      <Calendar onChange={onChange} value={value} />
-    </div>
+    <Popup>
+      <Section>
+        <Calendar onChange={onChange} value={value} />
+      </Section>
+    </Popup>
   );
 };
 
 export default FilterCalendar;
+const Section = styled.section`
+  //Calendar is class used inside react-calendar
+  .react-calendar {
+    width: 100%;
+    border-radius: 0.5rem 0.5rem 0.5rem 0;
+    border-color: transparent;
+    background: var(--baseColor-Light);
+  }
+  .react-calendar__month-view__weekdays__weekday {
+    font-size: 1.1rem;
+  }
+  .react-calendar__tile:hover {
+    background: var(--baseColor-Dark);
+    color: var(--baseColor-Light);
+  }
+  .react-calendar__tile--active,
+  .react-calendar__tile--active:enabled:hover,
+  .react-calendar__tile--active:enabled:focus {
+    background: var(--baseColor-Dark);
+    color: var(--baseColor-Light);
+  }
+  .react-calendar__tile,
+  .react-calendar__month-view__days__day {
+    font-size: 1.5rem;
+  }
+`;
